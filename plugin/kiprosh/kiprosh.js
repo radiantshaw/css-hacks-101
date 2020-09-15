79bac6af3fc2885d3a791d5d5a8f12c7fbb6cd97
Nipun Paradkar <nipun@kiprosh.com>
2020-09-15 23:37:03 +0530

    Add backgrounds for initial slide

--- plugin/kiprosh/kiprosh.js
+++ plugin/kiprosh/kiprosh.js
@@ -3,6 +3,7 @@ var Kiprosh = (function() {
   function _createInitialSlide(deck) {
     var $initialTitle = document.createTextNode(document.title);
     var $initialHeading = document.createElement('h1');
+    $initialHeading.classList.add('kiprosh-intro-text');
     $initialHeading.appendChild($initialTitle);
     var $initialSlide = document.createElement('section');
     $initialSlide.appendChild($initialHeading);
@@ -15,13 +16,16 @@ var Kiprosh = (function() {
     var slides = deck.getSlides();
 
     slides.forEach(function(slide, index) {
-      if (index == 0) { return }
+      var state = 'kiprosh-slide';
+      if (index == 0) {
+        state = 'kiprosh-initial-slide';
+      }
 
       var attributeValue = slide.getAttribute('data-state');
       if (attributeValue == null) {
-        slide.setAttribute('data-state', 'kiprosh-slide');
+        slide.setAttribute('data-state', state);
       } else {
-        slide.setAttribute('data-state', attributeValue + ' kiprosh-slide');
+        slide.setAttribute('data-state', attributeValue + ' ' + state);
       }
     });
   }
