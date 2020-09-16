2523fb791485c4db8144c897a1378d6e051d7e58
Nipun Paradkar <nipun@kiprosh.com>
2020-09-16 22:57:44 +0530

    Use JS and HTML to create initial slide instead of CSS

--- plugin/kiprosh/kiprosh.js
+++ plugin/kiprosh/kiprosh.js
@@ -15,12 +15,32 @@ var Kiprosh = (function(callbacks) {
   return Kiprosh;
 })([
   function(deck) {
-    var $initialTitle = document.createTextNode(document.title);
-    var $initialHeading = document.createElement('h1');
-    $initialHeading.classList.add('kiprosh-intro-text');
-    $initialHeading.appendChild($initialTitle);
+    var $introSquareImage = document.createElement('img');
+    $introSquareImage.src = 'plugin/kiprosh/intro-square.svg';
+    $introSquareImage.width = 500;
+
+    var $logoImage = document.createElement('img');
+    $logoImage.src = 'plugin/kiprosh/logo.png';
+    $logoImage.width = 128;
+    $logoImage.style.position = 'absolute';
+    $logoImage.style.left = 'calc(50% - 64px)';
+    $logoImage.style.top = '15%';
+
+    var $mainHeading = document.createElement('h1');
+    $mainHeading.innerText = document.title;
+    $mainHeading.style.color = '#fff';
+    $mainHeading.style.fontSize = '1.3em';
+    $mainHeading.style.position = 'absolute';
+    $mainHeading.style.top = '50%';
+    $mainHeading.style.left = 'calc(50% - 210px)';
+    $mainHeading.style.width = '420px';
+    $mainHeading.style.maxHeight = '200px';
+    $mainHeading.style.overflow = 'hidden';
+
     var $initialSlide = document.createElement('section');
-    $initialSlide.appendChild($initialHeading);
+    $initialSlide.appendChild($introSquareImage);
+    $initialSlide.appendChild($logoImage);
+    $initialSlide.appendChild($mainHeading);
 
     var $slides = deck.getSlidesElement();
     $slides.prepend($initialSlide);
@@ -29,16 +49,13 @@ var Kiprosh = (function(callbacks) {
     var slides = deck.getSlides();
 
     slides.forEach(function(slide, index) {
-      var state = 'kiprosh-slide';
-      if (index == 0) {
-        state = 'kiprosh-initial-slide';
-      }
+      if (index == 0) { return }
 
       var attributeValue = slide.getAttribute('data-state');
       if (attributeValue == null) {
-        slide.setAttribute('data-state', state);
+        slide.setAttribute('data-state', 'kiprosh-slide');
       } else {
-        slide.setAttribute('data-state', attributeValue + ' ' + state);
+        slide.setAttribute('data-state', attributeValue + ' kiprosh-slide');
       }
     });
   }
